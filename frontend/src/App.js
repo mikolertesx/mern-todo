240cb5cdbddfdc302be1f7e8b1722a841da62524
Miguel Angel <mgangsal@outlook.com>
2020-09-27 21:14:11 -0500

    Created the starting project.

---
+++ frontend/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>
+          React is cool for MERN apps.
+        </h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;

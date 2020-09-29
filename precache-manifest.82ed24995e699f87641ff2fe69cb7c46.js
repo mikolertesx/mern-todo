dde8471cbaf48e98d61a35ef25f4763e5474c381
Miguel Angel <mgangsal@outlook.com>
2020-09-28 20:22:14 -0500

    Updates

---
+++ precache-manifest.82ed24995e699f87641ff2fe69cb7c46.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2672faa65b6d40fed38abd4a5fa67503",
+    "url": "/mern-todo/index.html"
+  },
+  {
+    "revision": "cdd25197fd9153cee752",
+    "url": "/mern-todo/static/css/main.17b6cb70.chunk.css"
+  },
+  {
+    "revision": "c5de1704de413f6c0df4",
+    "url": "/mern-todo/static/js/2.942d624c.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/mern-todo/static/js/2.942d624c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cdd25197fd9153cee752",
+    "url": "/mern-todo/static/js/main.d1cf3064.chunk.js"
+  },
+  {
+    "revision": "f5011a74aced60903ce8",
+    "url": "/mern-todo/static/js/runtime-main.2ea91912.js"
+  }
+]);
